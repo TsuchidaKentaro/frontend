68be32f5d315a715b0847d056f6f958e5076b2eb
ImaiShibuki <shibuki.imai@g.dreamcareer.co.jp>
2024-06-26 16:47:48 +0900

    storybook

---
+++ src/stories/Circle.stories.js
@@ -0,0 +1,10 @@
+import { Meta } from "@storybook/react";
+import Circle from "./Circle.jsx";
+
+const meta = {
+    component: Circle,
+    title: "Circle",
+};
+
+export default meta;
+export const BaseCircle = {};
