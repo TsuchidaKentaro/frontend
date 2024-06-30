ad8690bb53b76ca171dc49e5160a6ae2081fbd47
Gomibuchi-Ryoma <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-06-30 10:36:24 +0900

    ESLint、Prettier追加

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 /*App.js*/
 
 import { RecoilRoot } from 'recoil';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import CharacterCounter from './components/CharacterCounter/CharacterCounter';
 import TodoApp from './components/TodoApp/index';
-
 
 const App = () => {
   const [fruits, setFruits] = useState(null);
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("/api");
+        const res = await fetch('/api');
         const json = await res.json();
         setFruits(json);
       } catch (e) {
@@ -23,11 +21,11 @@ const App = () => {
     fetchData();
   }, []);
 
-
   return (
     <div className="container fruitsList">
       <h1>Fruits Store</h1>
 
+      <h1></h1>
 
       {fruits?.map((fruit) => (
         <div key={fruit.id}>
